836551f0cfce35af49fce52228893bb063eece13
Olivier Duhart <olivier.duhart@sage.com>
2020-06-18 11:11:20 +0200

    wip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
+var fs = require('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,18 +11,46 @@ try {
   console.log(`output ${outputFormat}`);
   const testProject = core.getInput('testProject');
   console.log(`output ${testProject}`);
+  const excludes = core.getInput('excludes');
+  console.log(`excludes ${excludes}`);
 
-    exec("ls -la", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+/****************************************/
+/****                                ****/
+/****  creer un fichier msbuild.rsp  ****/
+/****                                ****/
+/****************************************/
+
+let msbuild = `/p:coverletOutput=${output} /p:CollectCoverage=true /p:CoverletOutputFormat=${outputFormat}`
+if (excludes !== null && excludes !== undefined) {
+    msbuild += ` /:exclude="${excludes}"`;    
+}
+msbuild += ` ${testProject}`
+
+fs.writeFileSync('msbuild.rsp',msbuild);
+
+
+
+/* ***************************************/
+/* ***                                ****/
+/* ***  lancer dotnet test            ****/
+/* ***                                ****/
+/* ***************************************/
+
+var ls = execSync('ls -la');
+console.log(ls.toString());
+
+var dotnet = execSync('dotnet test -c Debug');
+console.log(dotnet.toString());
+
+/****************************************/
+/****                                ****/
+/****  supprimer msbuild.rsp         ****/
+/****                                ****/
+/****************************************/
+
+if (fs.existsSync('msbuild.rsp')) {
+    fs.unlink('msbuild.rsp')
+}
 
     exec("dotnet --info", (error, stdout, stderr) => {
         if (error) {
@@ -39,5 +68,8 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
+    if (fs.existsSync('msbuild.rsp')) {
+        fs.unlink('msbuild.rsp')
+    }
   core.setFailed(error.message);
 }
