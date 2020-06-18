5bea70c473fc046570fbf24dc68cf814b3decbb9
Olivier Duhart <olivier.duhart@sage.com>
2020-06-18 10:15:42 +0200

    test exec

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const cli = require('cli');
+const { exec } = require("child_process");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const output = core.getInput('output');
-  console.log(`output ${output}!`);
+  console.log(`output ${output}`);
   const outputFormat = core.getInput('outputFormat');
-  console.log(`output ${outputFormat}!`);
+  console.log(`output ${outputFormat}`);
   const testProject = core.getInput('testProject');
-  console.log(`output ${testProject}!`);
+  console.log(`output ${testProject}`);
+
+    exec("ls -la", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("dotnet --info", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
