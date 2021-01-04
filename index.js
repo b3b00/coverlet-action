b5db10e85055940c2f46ae191320ab4d9ea49300
Olivier Duhart <olivier.duhart@sage.com>
2021-01-04 15:48:53 +0100

    excludes ?

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ try {
   const testProject = core.getInput("testProject");
   let excludestring = core.getInput("excludes");
   let includestring = core.getInput("excludes");
+  
 
   /****************************************/
   /****                                ****/
@@ -19,18 +20,10 @@ try {
 
   console.log("create coverlet args");
 
-  let msbuild = `/p:coverletOutput=${output} /p:CollectCoverage=true /p:CoverletOutputFormat=${outputFormat}`;
+  let msbuild = `/p:coverletOutput=${output} -p:CollectCoverage=true -p:CoverletOutputFormat=${outputFormat}`;
   if (excludestring !== null && excludestring !== undefined) {
-    msbuild += ` /p:Exclude=${excludestring}`;
-    console.log(`found exclusions ${excludestring}`);
-    // let excludes = excludestring.split(',');
-    // if (excludes !== null && excludes !== undefined) {
-    //   console.log(`found ${excludes.length} exclusions`);
-    //   for (let i = 0; i < excludes.length; i++) {
-    //     console.log(`add exclusion ${excludes[i]}`);
-    //     msbuild += ` /p:Exclude=${excludes[i]}`;
-    //   }
-    // }
+    msbuild += ` -p:Exclude=\\"${excludestring}\\"`;
+    console.log(`found exclusions ${excludestring}`);    
   }
   msbuild += ` ${testProject}`;
 
